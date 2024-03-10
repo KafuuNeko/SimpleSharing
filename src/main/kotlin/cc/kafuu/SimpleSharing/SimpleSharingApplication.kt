b10913d59823823710ceba31cb10420f0e355e27
Kafuu Neko <kafuuneko@gmail.com>
2024-03-10 19:10:05 +0800

    Initialize Project

---
+++ src/main/kotlin/cc/kafuu/SimpleSharing/SimpleSharingApplication.kt
@@ -0,0 +1,11 @@
+package cc.kafuu.SimpleSharing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleSharingApplication
+
+fun main(args: Array<String>) {
+	runApplication<SimpleSharingApplication>(*args)
+}
---
+++ src/test/kotlin/cc/kafuu/SimpleSharing/SimpleSharingApplicationTests.kt
@@ -0,0 +1,13 @@
+package cc.kafuu.SimpleSharing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleSharingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
